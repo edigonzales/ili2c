78a0d157466723f8cc2473788440df78c6a9a311
Stefan Ziegler <edi.gonzales@gmail.com>
2025-10-16 09:23:26 +0200

    Remove vendored ANTLR runtime

--- python/ili2c/pyili2c/parser/__init__.py
+++ python/ili2c/pyili2c/parser/__init__.py
@@ -1,9 +1,5 @@
 """INTERLIS parser entry points."""
 
-from .._vendor import ensure_antlr4_runtime
-
-ensure_antlr4_runtime()
-
-from .core import ParserSettings, parse  # noqa: E402,F401
+from .core import ParserSettings, parse  # noqa: F401
 
 __all__ = ["ParserSettings", "parse"]
